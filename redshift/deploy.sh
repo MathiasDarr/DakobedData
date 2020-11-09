b83740e35dca76e50c3878a9a725c433017680a2
mddarr <mddarr@gmail.com>
2020-11-09 15:50:37 -0800

    Use psycopg2 to connect to the redshift cluster

--- redshift/deploy.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-aws cloudformation deploy --template redshift_template.yml --stack-name redhisft-stack --parameter-overrides DataBucketName=new-dakobed-redshift-cluster --capabilities CAPABILITY_IAM
